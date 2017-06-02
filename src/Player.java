1bf47b0820ccca0752fb5659b12dd9af2c9efea3
Matheus Lourenço <theusoares_rj@hotmail.com>
2017-06-02 16:07:19 -0300

    player class changes

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,45 @@
 
 public class Player {
+    
+    int fila;
+    int coluna;
+    Character personagem;
+    
+    public enum Character{
+        
+        Green, Mustard, Peacock, Plum, Scarlet, White;
+    }
+    
+    public Player (Character personagem){
+        
+        this.personagem = personagem;
+        
+        switch (personagem) {
+                
+            case Green:
+                fila=14;
+                coluna=0;
+                
+            case Mustard:
+                fila=0;
+                coluna=17;
+                
+            case Peacock:
+                fila=24;
+                coluna=6;
+                
+            case Plum:
+                fila=24;
+                coluna=19;
+                
+            case Scarlet:
+                fila=7;
+                coluna=14;
+                
+            case White:
+                fila=9;
+                coluna=0;
+        }
+    }
 
 }
